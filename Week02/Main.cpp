41f737c1c3444566d078510fe8e064ac2f728dc1
Nam Mai <mdnam2410@gmail.com>
2020-11-13 08:28:50 +0700

    Modify Main.cpp

--- Week02/Main.cpp
+++ Week02/Main.cpp
@@ -6,8 +6,17 @@
 
 #include <iostream>
 
+void press_enter_to_continue()
+{
+    std::cout << "Nhan enter de tiep tuc: ";
+    char c;
+    std::cin.get(c);
+    std::cout << '\n';
+}
+
 void illustrate_point()
 {
+    std::cout << "# class Point\n";
     Point* a = new Point(2, 4);
     Point* b = new Point(-12, 0);
 
@@ -25,6 +34,7 @@ void illustrate_point()
 
 void illustrate_line()
 {
+    std::cout << "# class Line\n";
     Line* l1 = new Line{ Point(1, 3), Point(3, 4) };
     Line* l2 = new Line{ Point(6, 0), Point(17, 23) };
 
@@ -41,6 +51,7 @@ void illustrate_line()
 
 void illustrate_rectangle()
 {
+    std::cout << "# class Rectangle\n";
     Rectangle* r1 = new Rectangle;
     Rectangle* r2 = new Rectangle(Point(1, 8), Point(2, 10));
 
@@ -55,17 +66,71 @@ void illustrate_rectangle()
     std::cout << "So doi tuong sau khi da xoa r2: " << Rectangle::s_instance_count << '\n';
 }
 
+void illustrate_random()
+{
+    std::cout << "# class Random\n";
+    Random r;
+    
+    std::cout << "So duoc tao ngau nhien: " << r.next() << '\n';
+
+    int max;
+    std::cout << "Nhap max: ";
+    std::cin >> max;
+    std::cin.ignore();
+
+    std::cout << "So duoc tao ngau nhien trong khoang [0, " << max - 1 << "]: " << r.next(max) << '\n';
+}
+
+void print_roll_result(Dice& dice, int number_of_rolls)
+{
+    for (int i = 0; i < number_of_rolls; ++i)
+        std::cout << dice.roll() << (i == number_of_rolls - 1 ? '\n' : ' ');
+}
+
+void illustrate_dice()
+{
+    std::cout << "# class Dice\n";
+    Dice d1, d2, d3;
+    std::cout << "Da tao ra 3 con xi ngau d1, d2, d3.\n";
+
+    int max_roll_time = 15;
+    Random number_of_rolls;
+
+    std::cout << "Xi ngau 1: ";
+    print_roll_result(d1, number_of_rolls.next(max_roll_time) + 1);
+    std::cout << "So lan lac cua xi ngau 1: " << d1.roll_times() << '\n';
+
+    std::cout << "Xi ngau 2: ";
+    print_roll_result(d2, number_of_rolls.next(max_roll_time) + 1);
+    std::cout << "So lan lac cua xi ngau 2: " << d2.roll_times() << '\n';
+
+    std::cout << "Xi ngau 3: ";
+    print_roll_result(d3, number_of_rolls.next(max_roll_time) + 1);
+    std::cout << "So lan lac cua xi ngau 3: " << d3.roll_times() << '\n';
+}
+
 int main()
 {
-    // srand((unsigned int) time(nullptr));
+    srand((unsigned int) time(nullptr));
 
     // Dice d;
     // for (int i = 0; i < 103; ++i)
     //     std::cout << d.roll() << (i == 102 ? '\n' : ' ');
     // std::cout << d.roll_times() << '\n';
 
-    //illustrate_point();
-    //illustrate_line();
-    //illustrate_rectangle();
+    illustrate_point();
+    press_enter_to_continue();
+
+    illustrate_line();
+    press_enter_to_continue();
+
+    illustrate_rectangle();
+    press_enter_to_continue();
+
+    illustrate_random();
+    press_enter_to_continue();
+
+    illustrate_dice();
+
     return 0;
 }
